8f567c3b57f1bee74d94c07d0ffb2e4228fc5366
Danil Ovsyannikov <70640782+yngghostt@users.noreply.github.com>
2022-06-29 02:28:32 +0300

    Добавлены функции

--- vm.c
+++ vm.c
@@ -1,6 +1,7 @@
 #include <limits.h>
 #include <float.h>
 #include <stdlib.h>
+#include <math.h>
 
 double **dynamic_array_alloc( size_t N, size_t M )
 {
@@ -21,7 +22,180 @@ void dynamic_array_free( double **A, size_t N )
     free(A);
 }
 
-double dot( double *x, double *y, int n )
+double sum( const double *x, int n )
+{
+    double res = 0;
+    int i;
+    for( i = 0; i < n; i++ ){
+        res += x[i];
+    }
+    return res;
+}
+
+double *zeros( int n)
+{
+    double *res = (double*)malloc(n*sizeof(double));
+    int i;
+    for( i = 0; i < n; i++ ){
+        res[i] = 0;
+    }
+    return res;
+
+}
+
+double *ones( int n )
+{
+    double *res = (double*)malloc(n*sizeof(double));
+    int i;
+    for( i = 0; i < n; i++ ){
+        res[i] = 1;
+    }
+    return res;
+}
+
+double *random_array( int n, double min, double max )
+{
+    double *res = (double*)malloc(n*sizeof(double));
+    int i;
+    for( i = 0; i < n; i++ ){
+        res[i] = (((double)rand())/RAND_MAX)*(max - min) + min;
+    }
+    return res;
+}
+
+double *random_real_array( int n )
+{
+    double *res = (double*)malloc(n*sizeof(double));
+    int i;
+    for( i = 0; i < n; i++ ){
+        res[i] = ((double)rand())/RAND_MAX;
+    }
+    return res;
+}
+
+double *range( double start, double stop )
+{
+    int n = (int)(start+stop) + 1;
+    double *res = (double*)malloc(n*sizeof(double));
+    int i = 0;
+    while (start + i < stop)
+    {
+        res[i] = start + i;
+        i++;
+    }
+    return res;
+}
+
+double *arange( double start, double stop, double step )
+{
+    int n = (int)((start+stop)/step) + 1;
+    double *res = (double*)malloc(n*sizeof(double));
+    int i = 0;
+    while (start+i*step < stop)
+    {
+        res[i] = start+i*step;
+        i++;
+    }
+
+    return res;
+}
+
+double *array_sin( const double *x, int n)
+{
+    double *res = (double*)malloc(n*sizeof(double));
+    int i;
+    for (i = 0; i < n; i ++)
+        res[i] = sin(x[i]);
+    return res;
+}
+
+double *array_cos( const double *x, int n)
+{
+    double *res = (double*)malloc(n*sizeof(double));
+    int i;
+    for (i = 0; i < n; i ++)
+        res[i] = cos(x[i]);
+    return res;
+}
+
+double *array_exp( const double *x, int n)
+{
+    double *res = (double*)malloc(n*sizeof(double));
+    int i;
+    for (i = 0; i < n; i ++)
+        res[i] = exp(x[i]);
+    return res;
+}
+
+double *array_ln( const double *x, int n)
+{
+    double *res = (double*)malloc(n*sizeof(double));
+    int i;
+    for (i = 0; i < n; i ++)
+        if (x[i] > 0)
+            res[i] = log(x[i]);
+        else
+            res[i] = NAN;
+
+    return res;
+}
+
+double *array_abs( const double *x, int n)
+{
+    double *res = (double*)malloc(n*sizeof(double));
+    int i;
+    for (i = 0; i < n; i ++)
+        if (x[i] > 0)
+            res[i] = x[i];
+        else
+            res[i] = -1*x[i];
+
+    return res;
+}
+
+double *array_power2( const double *x, int n)
+{
+    double *res = (double*)malloc(n*sizeof(double));
+    int i;
+    for (i = 0; i < n; i ++)
+        res[i] = x[i]*x[i];
+    return res;
+}
+
+double *array_power3( const double *x, int n)
+{
+    double *res = (double*)malloc(n*sizeof(double));
+    int i;
+    for (i = 0; i < n; i ++)
+        res[i] = x[i]*x[i]*x[i];
+    return res;
+}
+
+double *array_power_n( const double *x, int n, int power)
+{
+    double *res = (double*)malloc(n*sizeof(double));
+    int i;
+    for (i = 0; i < n; i ++)
+        if (x[i] == 0 && power == -1)
+            res[i] = NAN;
+        else
+            res[i] = pow(x[i], power);
+    return res;
+}
+
+double *array_sqrt( const double *x, int n )
+{
+    double *res = (double*)malloc(n*sizeof(double));
+    int i;
+    for (i = 0; i < n; i ++)
+        if (x[i] < 0)
+            res[i] = NAN;
+        else
+            res[i] = sqrt(x[i]);
+    return res;
+}
+
+double dot( const double *x, double *y, int n )
 {
     double res = 0;
     int i;
@@ -33,8 +207,9 @@ double dot( double *x, double *y, int n )
 
 }
 
-void mxv( double **m,  double *v, double *res, int rows, int cols )
+double *mxv( double **m,  double *v, int rows, int cols )
 {
+    double *res = (double*)malloc(rows*sizeof(double));
     int i;
     for (i = 0; i < rows; i++)
     {
@@ -42,39 +217,46 @@ void mxv( double **m,  double *v, double *res, int rows, int cols )
     }
 }
 
-void vxv( double *x, double *y, double **res, int n, int m )
+double **vxv( double *x, double *y, int n, int m )
 {
+    double **res = dynamic_array_alloc(m, n);
     int i, j;
     for (i = 0; i < n; i++)
         for(j = 0; j < m; j++)
             res[i][j] = x[i]*y[j];
 }
 
-void plus( double *x, double *y, double *res, int n )
+double *plus( double *x, double *y, int n )
 {
+    double *res = (double*)malloc(n*sizeof(double));
     int i;
     for (i = 0; i < n; i++)
     {
         res[i] = x[i]+y[i];
     }
+    return res;
 }
 
-void minus( double *x, double *y, double *res, int n )
+double *minus( double *x, double *y, int n )
 {
+    double *res = (double*)malloc(n*sizeof(double));
     int i;
     for (i = 0; i < n; i++)
     {
         res[i] = x[i]-y[i];
     }
+    return res;
 }
 
-void multiply( double *x, double a, double *res, int n )
+double *multiply( double *x, double a, int n )
 {
+    double *res = (double*)malloc(n*sizeof(double));
     int i;
     for (i = 0; i < n; i++)
     {
         res[i] = x[i]*a;
     }
+    return res;
 }
 double **transpose( double **m, int rows, int columns )
 {
@@ -82,7 +264,7 @@ double **transpose( double **m, int rows, int columns )
     int i, j;
     for(i = 0; i < rows; i++)
         for(j = 0; j < columns; j++)
-            res[i][j] = m[j][i];
+            res[j][i] = m[i][j];
 
     return res;
 }
@@ -99,7 +281,7 @@ int max_ind( const double *x, int n )
     return j;
 }
 
-int max( const double *x, int n )
+double max( const double *x, int n )
 {
     int i;
     double max = DBL_MIN;
@@ -122,7 +304,7 @@ int min_ind( const double *x, int n )
     return j;
 }
 
-int min( const double *x, int n )
+double min( const double *x, int n )
 {
     int i;
     double min = DBL_MAX;
@@ -142,7 +324,7 @@ int contains( const double *array, int x , int n )
     return 0;
 }
 
-double binarysearch( int a, const double *array, int n )
+double search( int a, const double *array, int n )
 {
     int low, high, middle;
     low = 0;
@@ -160,12 +342,12 @@ double binarysearch( int a, const double *array, int n )
     return -1;
 }
 
-void sort(double *a, int l, int r)
+void private_sort( double *a, int l, int r )
 {
     if (l == r) return;
     int mid = (l + r) / 2;
-    sort(a, l, mid);
-    sort(a, mid + 1, r);
+    private_sort(a, l, mid);
+    private_sort(a, mid + 1, r);
     int i = l;
     int j = mid + 1;
     double *tmp = (double *)malloc(r * sizeof(double ));
@@ -183,12 +365,17 @@ void sort(double *a, int l, int r)
 
     for (step = 0; step < r - l + 1; step++)
         a[l + step] = tmp[step];
-    free(tmp);
+    //free(tmp);
+}
+
+void sort( double *x, int n )
+{
+    private_sort(x, 0, n-1);
 }
 
 double *reverse( double *x, int n )
 {
-    double *res = (double *)malloc(n * sizeof(double));
+    double *res = (double*)malloc(n * sizeof(double));
     int i;
     for(i = 0; i < n; i++)
         res[i] = x[n-i-1];
